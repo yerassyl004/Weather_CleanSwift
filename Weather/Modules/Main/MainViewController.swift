9cdceb38d06dc90bb4bb3c8577eb74498a84e853
yerassyl004 <erasil.er94@gmail.com>
2024-05-15 07:54:56 +0500

    fixes

--- Weather/Modules/Main/MainViewController.swift
+++ Weather/Modules/Main/MainViewController.swift
@@ -306,15 +306,12 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let data = cellModel[indexPath.row]
         cell.configure(viewModel: data)
         cell.backgroundColor = UIColor(named: "cloudColor")
+        cell.selectionStyle = .none
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
@@ -339,7 +336,6 @@ extension ViewController: MainDisplayLogic {
             self.currentWeatherLabel.text = currentForecast.description
         }
         self.hourlyForecast = forecast
-        print("hourlyForecast \(hourlyForecast)")
         collectionView.reloadData()
     }
 }
